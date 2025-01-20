a1587dee5d295dea172037f35492990b955da4e9
Uman Sheikh <l1f21bsse0611@ucp.edu.pk>
2025-01-20 09:07:52 +0500

    Initial files

---
+++ init.sh
@@ -0,0 +1,9 @@
+echo "Running initil git configuration files";
+git init
+git config --local user.name "Uman Sheikh"
+git config --local user.email "l1f21bsse0611@ucp.edu.pk"
+mkdir src
+mkdir style
+touch ./src/index.html
+touch ./style/index.css
+npm init -y
